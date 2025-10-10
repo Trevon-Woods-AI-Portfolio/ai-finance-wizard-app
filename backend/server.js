1b14a8698b9534abee4dee3228af941cad3e3014
Trevon-Woods-AI-Portfolio <fullstackvon@gmail.com>
2025-10-10 11:21:39 -0500

    added backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./db/connectDB.js";
+import { server, app } from "./socket/socket.js";
+
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 5656;
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// app.use("/api/user", userRoutes)
+// app.use("/api/result", resultsRoutes)
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+});
